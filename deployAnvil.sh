f46ce88d5deb705eca1037f4033c0d02b943fb39
Epsilon <epsilon@scholes.xyz>
2024-05-24 12:18:48 +0200

    :recycle: Make the deployment scripts POSIX
    Make sure the deployment scripts work on any POSIX platform:
    - use `sh` rather than `zsh`
    - replace the `source` shell built-in by the POSIX `.` equivalent
    - do not source the `push_artifacts.sh` script but explicitly execute it

--- deployAnvil.sh
+++ deployAnvil.sh
@@ -1,9 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
+
+set -e
 
 # Run anvil.sh in another shell before running this
 
 # To load the variables in the .env file
-source .env
+. ./.env
 
 # Create some test orders
 export CREATE_TEST_ORDERS="yes"
@@ -11,7 +13,7 @@ export CREATE_TEST_ORDERS="yes"
 # To deploy and verify our contract
 forge script script/DeployAnywhere.s.sol:Deploy --rpc-url "http://127.0.0.1:8545/" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -v
 
-source push_artifacts.sh "DeployAnywhere.s.sol/11155111"
+./push_artifacts.sh "DeployAnywhere.s.sol/11155111"
 
 # cd web
 # npm run build
